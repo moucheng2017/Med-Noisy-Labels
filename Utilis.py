455a5359a2dcbb5ebad7201f10edf285c92d142d
Georgios <gathanasiou@iiia.csic.es>
2023-03-14 14:16:25 +0100

    dice check

--- Utilis.py
+++ Utilis.py
@@ -1484,7 +1484,11 @@ def segmentation_scores(label_trues, label_preds, n_class):
     :return:
     '''
     print("true labels:", type(label_trues))
+    print(label_trues.shape)
+    print(label_trues.max())
     print("preds:", type(label_preds))
+    print(label_preds.shape)
+    print(label_preds.max())
     assert len(label_trues) == len(label_preds)
 
     if n_class == 2:
