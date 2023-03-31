28c16052f5e8ce423b4670ff9b0e46c7260da023
Georgios <gathanasiou@iiia.csic.es>
2023-03-31 18:01:51 +0200

    hp

--- Run.py
+++ Run.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
                 train_batchsize=2,
                 validate_batchsize=1,
                 num_epochs=30,
-                learning_rate=1e-3,
+                learning_rate=1e-4,
                 alpha=0.0,
                 width=32,
                 depth=3,
