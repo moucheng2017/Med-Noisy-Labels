a76091a0dc8fe66fdd931ae6c801d4b5df747b7d
Georgios <gathanasiou@iiia.csic.es>
2023-04-03 15:23:45 +0200

    decrease depth

--- Models.py
+++ Models.py
@@ -312,7 +312,6 @@ def double_conv(in_channels, out_channels, step, norm):
             nn.Conv2d(out_channels, out_channels, 3, stride=1, padding=1, groups=1, bias=False),
             nn.InstanceNorm2d(out_channels, affine=True),
             nn.PReLU(),
-            nn.Dropout(0.2)
         )
     elif norm == 'bn':
         return nn.Sequential(
--- Train_ours.py
+++ Train_ours.py
@@ -198,7 +198,7 @@ def trainSingleModel(model_seg,
     #
     writer = SummaryWriter(path_name + '/Log/Log_' + model_name)
 
-    model_seg_stepwise = True
+    model_seg_stepwise = False
 
     if model_seg_stepwise == True:
 
