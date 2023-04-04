d2e911e0a3147773e3ee960dd2e6f4cd675ab5b0
Georgios <gathanasiou@iiia.csic.es>
2023-04-04 12:58:15 +0200

    CMs saving

--- Train_GCM.py
+++ Train_GCM.py
@@ -397,6 +397,10 @@ def trainSingleModel(model_seg,
                 #labels_all.append(labels_avrg)
                 #
                 outputs_logits, outputs_logits_noisy = model_seg(images)
+                print("CMs number: ", len(outputs_logits_noisy))
+                print("CM type: ", type(outputs_logits_noisy))
+                print("CM shape: ", outputs_logits_noisy[0].shape)
+                break
                 #
                 loss, loss_ce, loss_trace = noisy_label_loss(outputs_logits, outputs_logits_noisy, labels_all, alpha)
                 # if low_rank_mode is False:
