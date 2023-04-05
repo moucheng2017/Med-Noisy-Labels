1f13783bb2a7f89cb079080e323635fe1fa1b257
Georgios <gathanasiou@iiia.csic.es>
2023-04-05 13:26:45 +0200

    skin

--- Run.py
+++ Run.py
@@ -136,7 +136,7 @@ if __name__ == '__main__':
                 train_batchsize = 2,
                 validate_batchsize = 1,
                 num_epochs = 100,
-                learning_rate = 1e-2,
+                learning_rate = 1e-5,
                 width = 32,
                 depth = 3,
                 augmentation='all_flip',
