818952fe40736879ac28385fade45168253b19ff
Georgios <gathanasiou@iiia.csic.es>
2023-04-10 10:09:47 +0200

    skin wh

--- Utilis.py
+++ Utilis.py
@@ -1009,6 +1009,7 @@ class CustomDataset_punet(torch.utils.data.Dataset):
                 #
                 image = tiff.imread(all_images[index])
                 image = np.array(image, dtype='float32')
+                print("image shape: ", image.shape)
 
                 label_AR[label_AR == 4.0] = 3.0
                 label_SG[label_SG == 4.0] = 3.0
