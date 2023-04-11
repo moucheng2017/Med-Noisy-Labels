74ed5eb2aba7a1fbeb9eb4343c2f6ee902661d73
Georgios <gathanasiou@iiia.csic.es>
2023-04-11 16:09:30 +0200

    GCM COC d5

--- Train_GCM.py
+++ Train_GCM.py
@@ -225,23 +225,23 @@ def trainSingleModel(model_seg,
 
         ### Encoders - GRAD ###
         for layer in model_seg.encoders.parameters():
-            layer.requires_grad = True
+            layer.requires_grad = False
         ### =============== ###
 
         ### Decoders - GRAD ###
         for param in model_seg.decoders.parameters():
-            param.requires_grad = True
-        ### =============== ###
-
-        ### Decoders CMs - GRAD ###
-        for param in model_seg.decoders_noisy_layers.parameters():
             param.requires_grad = False
-        ### =================== ###
+        ### =============== ###
 
         ### Last Conv - GRAD ###
         for param in model_seg.conv_last.parameters():
-            param.requires_grad = True
+            param.requires_grad = False
         ### ================ ###
+
+        ### Decoders CMs - GRAD ###
+        for param in model_seg.decoders_noisy_layers.parameters():
+            param.requires_grad = True
+        ### =================== ###
 
        
 
