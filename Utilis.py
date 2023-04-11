c4c82988b1b189d766b90224af1f80afb116bbcb
Georgios <gathanasiou@iiia.csic.es>
2023-04-11 12:46:29 +0200

    unet cocs

--- Utilis.py
+++ Utilis.py
@@ -60,7 +60,8 @@ class CustomDataset(torch.utils.data.Dataset):
 
             label = np.array(label, dtype='float32')
             image = np.array(image, dtype='float32')
-
+        print(image.max())
+        print(label.max())
         #
         # # label = Image.open(all_labels[index])
         # # label = tiff.imread(all_labels[index])
