3b9af2c24c2c38350a1e2da55a7e6ae796c432b5
Georgios <gathanasiou@iiia.csic.es>
2023-04-11 13:08:55 +0200

    GCM COC d5

--- Utilis.py
+++ Utilis.py
@@ -1011,6 +1011,9 @@ class CustomDataset_punet(torch.utils.data.Dataset):
                 image = tiff.imread(all_images[index])
                 image = np.array(image, dtype='float32') / 255.
 
+                print(image.max())
+                print(label_AR.max())
+
                 label_AR[label_AR == 4.0] = 3.0
                 label_SG[label_SG == 4.0] = 3.0
                 label_avrg[label_avrg == 4.0] = 3.0
