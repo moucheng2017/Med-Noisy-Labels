a2535bb97b464cd5b68eb821cd5b1931c36d57e6
Georgios <gathanasiou@iiia.csic.es>
2023-04-12 10:47:31 +0200

    labels=0

--- Train_ours.py
+++ Train_ours.py
@@ -498,8 +498,10 @@ def trainSingleModel(model_seg,
                                                                class_no=class_no)
                     #
                     total_train_loss.append((running_loss / (j + 1)).cpu().detach().numpy())
-                    total_ce_loss.append((running_loss_ce / (j + 1)).cpu().detach().numpy())
-                    total_trace_loss.append((running_loss_trace / (j + 1)).cpu().detach().numpy())
+                    #total_ce_loss.append((running_loss_ce / (j + 1)).cpu().detach().numpy())
+                    #total_trace_loss.append((running_loss_trace / (j + 1)).cpu().detach().numpy())
+                    total_ce_loss.append(0.0)
+                    total_trace_loss.append(0.0)
 
                     train_dice.append(running_iou / (j + 1))
                     valid_dice.append(v_dice)
