a9d0f2ed0d9c0bb99caa452b4c524c84678d854b
Georgios <gathanasiou@iiia.csic.es>
2023-04-18 11:49:34 +0200

    remove noise

--- Train_GCM.py
+++ Train_GCM.py
@@ -522,7 +522,7 @@ def trainSingleModel(model_seg,
                 # train_iou = segmentation_scores(labels_avrg.cpu().detach().numpy(), train_output.cpu().detach().numpy(), class_no)
                 #
                 # train_iou = dice_coef_default(outputs_logits, labels_avrg)
-                train_iou = dice_coef_default(outputs_logits, labels_avrg)
+                train_iou = dice_coef_custom(outputs_logits, labels_avrg)
                 
                 running_loss += loss
                 running_loss_ce += loss_ce
