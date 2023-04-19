1c491dce990257e5c91f275faf1b691bfbb6ceda
Georgios <gathanasiou@iiia.csic.es>
2023-04-19 09:11:21 +0200

    dice >1

--- Utilis.py
+++ Utilis.py
@@ -1728,7 +1728,7 @@ def segmentation_scores(label_trues, label_preds, n_class):
     
     # Compute the Dice coefficient
     dice = ((2 * area_intersection + 1e-6) / (area_union + 1e-6))
-    print("dice: ", dice)
+    # print("dice: ", dice)
     return dice[1]
 
 
