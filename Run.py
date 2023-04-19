7a7b97725cda4548b92a3c21bc0ea3ef5d35397a
Georgios <gathanasiou@iiia.csic.es>
2023-04-19 12:06:27 +0200

    dice >1

--- Run.py
+++ Run.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
                    repeat=1,
                    train_batchsize=2,
                    validate_batchsize=1,
-                   num_epochs=50,
+                   num_epochs=200,
                    learning_rate=1e-6,
                    input_height=192,
                    input_width=256,
