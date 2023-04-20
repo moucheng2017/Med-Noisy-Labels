050e3ba05584bcc32abf4aefaeb020ff803bf0cd
Georgios <gathanasiou@iiia.csic.es>
2023-04-20 22:26:30 +0200

    test batch

--- Utilis.py
+++ Utilis.py
@@ -636,17 +636,18 @@ def evaluate(evaluatedata, model, device, class_no):
             testimg = testimg.to(device=device, dtype=torch.float32)
             testlabel = testlabel.to(device=device, dtype=torch.float32)
             #
-            # print("val image: ", testname)
-            # print("val image size: ", testimg.size())
-            # print("val label size: ", testlabel.size())
+            print("val image: ", testname)
+            print("val image size: ", testimg.size())
+            print("val label size: ", testlabel.size())
             testoutput = model(testimg)
-            # print("val out size: ", testoutput.size())
+            print("val out size: ", testoutput.size())
             if class_no == 2:
                 # testoutput = torch.sigmoid(testoutput)
                 # testoutput = (testoutput > 0.5).float()
                 _, testoutput = torch.max(testoutput, dim=1)
             else:
                 _, testoutput = torch.max(testoutput, dim=1)
+            print("val max size: ", testoutput.size())
             #
             # mean_iu_ = segmentation_scores(testlabel.cpu().detach().numpy(), testoutput.cpu().detach().numpy(), class_no)
             plt.imsave('./test_results/' + testname[0] + '_segmented_max_0.png', testoutput[0].cpu().detach().numpy(), cmap = 'gray')
