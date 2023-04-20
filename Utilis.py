5eb75a9bd172a4ba67818763763f00f8ab5537a9
Georgios <gathanasiou@iiia.csic.es>
2023-04-20 14:31:47 +0200

    test batch

--- Utilis.py
+++ Utilis.py
@@ -639,16 +639,12 @@ def evaluate(evaluatedata, model, device, class_no):
             testoutput = model(testimg)
             if class_no == 2:
                 testoutput = torch.sigmoid(testoutput)
-                print("logits sig: ", testoutput.size())
                 testoutput = (testoutput > 0.5).float()
-                print("logits 0.5: ", testoutput.size())
-                _, testoutput = torch.max(testoutput, dim=1)
-                print("logits max: ", testoutput.size())
             else:
                 _, testoutput = torch.max(testoutput, dim=1)
             #
-            # mean_iu_ = segmentation_scores(testlabel.cpu().detach().numpy(), testoutput.cpu().detach().numpy(), class_no)
-            mean_iu_ = dice_coef_simplified(testoutput, testlabel)
+            mean_iu_ = segmentation_scores(testlabel.cpu().detach().numpy(), testoutput.cpu().detach().numpy(), class_no)
+            # mean_iu_ = dice_coef_simplified(testoutput, testlabel)
             # mean_iu_ = dice_coef_torchmetrics(testoutput, testlabel, 2, 'cuda')
             test_iou += mean_iu_
         #
